6701a387640e7fc39aee34b2e54f877f7cb8ee8c
bmorrell <benjamin.morrell@jpl.nasa.gov>
2019-07-26 21:09:48 -0700

    comments in PGV

--- internal/src/pose_graph_visualizer/src/PoseGraphVisualizer.cc
+++ internal/src/pose_graph_visualizer/src/PoseGraphVisualizer.cc
@@ -33,7 +33,10 @@ inline geometry_msgs::Point tfpoint2msg(const tf::Vector3 &v) {
 geometry_msgs::Point PoseGraphVisualizer::GetPositionMsg(
     long unsigned int key,
     const std::map<long unsigned int, tf::Pose>& poses) const {
-      ROS_INFO("In getPositionMsg");
+  ROS_INFO("In getPositionMsg");
+  if (poses.find(key) == poses.end()) {
+    ROS_ERROR("PGV: Key %lu does not exist in GetPositionMsg", key);
+  }
   return tfpoint2msg(poses.at(key).getOrigin());
 }
 
@@ -560,6 +563,7 @@ void PoseGraphVisualizer::MakeMenuMarker(const tf::Pose &position,
 void PoseGraphVisualizer::VisualizePoseGraph() {
   // Publish odometry edges.
   if (odometry_edge_pub_.getNumSubscribers() > 0) {
+    ROS_INFO("Odometry Edges");
     visualization_msgs::Marker m;
     m.header.frame_id = fixed_frame_id_;
     m.ns = fixed_frame_id_;
@@ -584,6 +588,7 @@ void PoseGraphVisualizer::VisualizePoseGraph() {
 
   // Publish loop closure edges.
   if (loop_edge_pub_.getNumSubscribers() > 0) {
+    ROS_INFO("Loop Edges");
     visualization_msgs::Marker m;
     m.header.frame_id = fixed_frame_id_;
     m.ns = fixed_frame_id_;
@@ -608,6 +613,7 @@ void PoseGraphVisualizer::VisualizePoseGraph() {
 
   // Publish artifact edges.
   if (artifact_edge_pub_.getNumSubscribers() > 0) {
+    ROS_INFO("Artifact Edges");
     visualization_msgs::Marker m;
     m.header.frame_id = fixed_frame_id_;
     m.ns = fixed_frame_id_;
@@ -642,6 +648,7 @@ void PoseGraphVisualizer::VisualizePoseGraph() {
 
   // Publish UWB edges.
   if (uwb_edge_pub_.getNumSubscribers() > 0) {
+    ROS_INFO("UWB Edges");
     visualization_msgs::Marker m;
     m.header.frame_id = fixed_frame_id_;
     m.ns = fixed_frame_id_;
@@ -822,6 +829,7 @@ void PoseGraphVisualizer::VisualizePoseGraph() {
 
   // Interactive markers.
   if (publish_interactive_markers_) {
+    ROS_INFO("Pose Graph Nodes");
     for (const auto &keyed_pose : keyed_poses_) {
       gtsam::Symbol key_id = gtsam::Symbol(keyed_pose.first);
       std::string robot_id = std::string(key_id);
