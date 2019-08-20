39ee721a835595a931606287f44092aefda13fef
user <yunchang@mit.edu>
2019-08-19 20:14:45 -0400

    bug fixes and tuning

--- internal/src/blam_slam/scripts/performance_eval.py
+++ internal/src/blam_slam/scripts/performance_eval.py
@@ -17,8 +17,8 @@ class PerformanceEvaluation:
         rospy.Subscriber('/blam_output', PoseStamped, self.get_blam_pose_clbk, queue_size=1)
         rospy.Subscriber('/odometry', Odometry, self.get_odom_clbk, queue_size=1)
 
-        self.lidar_slip_pub = rospy.Publisher(self.node_name + '/lidar_slip', Float64, queue_size=10)
-        self.slip_bool_pub = rospy.Publisher(self.node_name + '/is_slipping', Bool, queue_size=10)
+        self.lidar_slip_pub = rospy.Publisher(self.robot_namespace + 'lidar_slip', Float64, queue_size=1)
+        self.slip_bool_pub = rospy.Publisher(self.node_name + '/is_slipping', Bool, queue_size=1)
         rospy.loginfo("Performance evaluation node initialized!")
 
         self.last_blam_pose = None
@@ -35,14 +35,17 @@ class PerformanceEvaluation:
         self.pub_horizon = 900
 
     def increment_slip(self):
+    	# calculate odom traveled
 		odom_traveled = math.sqrt((self.odom_pose.position.x - self.slip_start_odom_pose.position.x)**2 + \
 			(self.odom_pose.position.y - self.slip_start_odom_pose.position.y)**2 + \
 			(self.odom_pose.position.z - self.slip_start_odom_pose.position.z)**2)
+		# calculate travled distance in blam
 		blam_traveled = math.sqrt((self.last_blam_pose.pose.position.x - self.slip_start_blam_pose.pose.position.x)**2 + \
 			(self.last_blam_pose.pose.position.y - self.slip_start_blam_pose.pose.position.y)**2 + \
 			(self.last_blam_pose.pose.position.z - self.slip_start_blam_pose.pose.position.z)**2)
 		slip = odom_traveled - blam_traveled
-		if slip > 0.0:
+		# only set sliiping to true if slip is greater than 0
+		if slip > 0.05:
 			self.slip = slip
 			self.slipping = True
 			return True
@@ -61,22 +64,20 @@ class PerformanceEvaluation:
 	    		(msg.pose.position.z - self.last_blam_pose.pose.position.z)**2)
 	    	# calculate speed
 	    	delta_t = (msg.header.stamp - self.last_blam_pose.header.stamp).to_sec()
-	    	speed_from_blam = trans_dist / float(delta_t)
-	    	# compare with odom speed
-	    	if abs(self.odom_speed - speed_from_blam) > self.threshold:
-	    		if self.increment_slip():
-	    			self.pub_cycle = 0
-	    	else:
-	    		self.slipping = False
+	    	if delta_t > 0:
+		    	speed_from_blam = trans_dist / float(delta_t)
+		    	# compare with odom speed
+		    	if abs(self.odom_speed - speed_from_blam) > self.threshold:
+		    		if self.increment_slip():
+		    			self.pub_cycle = 0
+		    	else:
+		    		self.slipping = False
+		    	# update last non slipping time
+		    	if not self.slipping:
+		    		self.slip_start_blam_pose = msg
+		    		self.slip_start_odom_pose = self.odom_pose
 
-	    	# if not slipping update last no slip place
-    		if not self.slipping:
-    			# keep track of when slip starts
-    			self.slip_start_odom_pose = self.odom_pose
-    			self.slip_start_blam_pose = msg
-
-    	else:
-    		self.last_blam_pose = msg
+    	self.last_blam_pose = msg
     	# publish the previous slip
     	self.slip_bool_pub.publish(self.slipping)
     	if self.slip > 0 and self.pub_cycle < self.pub_horizon: # don't publish insignificant slip dist
