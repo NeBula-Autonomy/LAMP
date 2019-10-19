acd93df07d02262496d101eb3129e5c5e515bc8d
Eric Heiden <heiden@usc.edu>
2019-10-19 01:33:33 +0000

    PoseGraph struct file saving/loading, message conversion

--- src/utils/include/utils/CommonStructs.h
+++ src/utils/include/utils/CommonStructs.h
@@ -1,11 +1,10 @@
 /*
  * Copyright Notes
  *
- * Authors: 
+ * Authors:
  * Alex Stephens       (alex.stephens@jpl.nasa.gov)
  * Benjamin Morrell    (benjamin.morrell@jpl.nasa.gov)
  */
-
 
 #ifndef COMMON_STRUCTS_H
 #define COMMON_STRUCTS_H
@@ -14,16 +13,23 @@
 #include <gtsam/base/Vector.h>
 #include <gtsam/geometry/Pose3.h>
 #include <gtsam/geometry/Rot3.h>
+#include <gtsam/inference/Symbol.h>
 #include <gtsam/linear/NoiseModel.h>
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
 #include <gtsam/nonlinear/Values.h>
-#include <gtsam/slam/PriorFactor.h>
 #include <gtsam/slam/BetweenFactor.h>
 #include <gtsam/slam/InitializePose3.h>
-#include <gtsam/inference/Symbol.h>
+#include <gtsam/slam/PriorFactor.h>
 
-#include <geometry_utils/Transform3.h>
 #include <geometry_utils/GeometryUtilsROS.h>
+#include <geometry_utils/Transform3.h>
+
+#include <pcl_ros/point_cloud.h>
+
+#include <geometry_msgs/PoseStamped.h>
+#include <pose_graph_msgs/PoseGraph.h>
+#include <pose_graph_msgs/PoseGraphEdge.h>
+#include <pose_graph_msgs/PoseGraphNode.h>
 
 // Typedef for 6x6 covariance matrices (x, y, z, roll, pitch, yaw).
 typedef geometry_utils::MatrixNxNBase<double, 6> Mat66;
@@ -33,25 +39,83 @@ typedef geometry_utils::MatrixNxNBase<double, 12> Mat1212;
 typedef gtsam::noiseModel::Gaussian Gaussian;
 typedef gtsam::noiseModel::Diagonal Diagonal;
 
-
 // GTSAM edge types
 typedef std::pair<gtsam::Symbol, gtsam::Symbol> Edge;
 typedef std::pair<gtsam::Symbol, gtsam::Symbol> ArtifactEdge;
 typedef std::pair<gtsam::Symbol, gtsam::Pose3> Prior;
 
+typedef pose_graph_msgs::PoseGraphNode GraphNode;
+typedef pose_graph_msgs::PoseGraphEdge GraphEdge;
+typedef pose_graph_msgs::PoseGraph::ConstPtr GraphMsgPtr;
+
+typedef std::vector<GraphEdge> EdgeMessages;
+typedef std::vector<GraphNode> NodeMessages;
+
+// Typedef for stored point clouds.
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+
+struct PoseGraph {
+  gtsam::Values values;
+  gtsam::NonlinearFactorGraph nfg;
+
+  std::string fixed_frame_id;
+
+  // Keep a list of keyed laser scans and keyed timestamps.
+  std::map<gtsam::Symbol, PointCloud::ConstPtr> keyed_scans;
+  std::map<gtsam::Symbol, ros::Time> keyed_stamps; // All nodes
+  std::map<double, gtsam::Symbol> stamp_to_odom_key;
+
+  // Message filters (if any)
+  std::string prefix;
+
+  // Initial key
+  gtsam::Symbol initial_key;
+
+  // Current key
+  gtsam::Symbol key;
+
+  EdgeMessages edges;
+  NodeMessages priors;
+
+  gtsam::Vector6 initial_noise{gtsam::Vector6::Zero()};
+
+  // Saves pose graph and accompanying point clouds to a zip file.
+  template <typename PGOSolver>
+  bool Save(const std::string& zipFilename, PGOSolver& solver) const;
+
+  // Loads pose graph and accompanying point clouds from a zip file.
+  template <typename PGOSolver>
+  bool Load(const std::string& zipFilename, PGOSolver& solver);
+
+  // Convert entire pose graph to message.
+  GraphMsgPtr ToMsg() const;
+
+  // Convert incremental pose graph with given values, edges and priors to
+  // message.
+  GraphMsgPtr ToMsg(const gtsam::Values& values,
+                    const EdgeMessages& edges,
+                    const NodeMessages& priors) const;
+
+  // Update from pose graph message.
+  bool FromMsg(const GraphMsgPtr& msg);
+};
+
 // Struct definition
 struct FactorData {
-  bool b_has_data;   // False if there is no data
-  std::string type;  // odom, artifact, loop clsoure
+  bool b_has_data;  // False if there is no data
+  std::string type; // odom, artifact, loop clsoure
   // Vector for possible multiple factors
-  std::vector<gtsam::Pose3> transforms;  // The transform (for odom, loop
-                                         // closures etc.) and pose for TS
-  std::vector<gtsam::SharedNoiseModel> covariances;      // Covariances for each transform
+  std::vector<gtsam::Pose3> transforms; // The transform (for odom, loop
+                                        // closures etc.) and pose for TS
+  std::vector<gtsam::SharedNoiseModel>
+      covariances; // Covariances for each transform
   std::vector<std::pair<ros::Time, ros::Time>>
-      time_stamps;  // Time when the measurement as acquired (first, second)
+      time_stamps; // Time when the measurement as acquired (first, second)
   // TODO - use ros::Time or something else?
 
-  std::vector<gtsam::Symbol> artifact_key;  // key for the artifacts
+  std::vector<gtsam::Symbol> artifact_key; // key for the artifacts
 };
 
 #endif
+
+#include "utils/SaveLoadGraph.hpp"
--- src/utils/src/PoseGraphMessageConversion.cc
+++ src/utils/src/PoseGraphMessageConversion.cc
@@ -1,43 +1,47 @@
-#include <pose_graph_serializer/serializer.h>
+#include "utils/CommonStructs.h"
+#include "utils/CommonFunctions.h"
 
-#include <parameter_utils/ParameterUtils.h>
-#include <utils/CommonFunctions.h>
-
-namespace pu = parameter_utils;
 namespace gu = geometry_utils;
 namespace gr = gu::ros;
 
-GraphPtr PoseGraphSerializer::ConvertToMsg(const PoseGraph& pose_graph) {
+
+GraphMsgPtr PoseGraph::ToMsg() const {
+  return ToMsg(values, edges, priors);
+}
+
+GraphMsgPtr PoseGraph::ToMsg(const gtsam::Values& values,
+                             const EdgeMessages& edges,
+                             const NodeMessages& priors) const {
   // Create the Pose Graph Message
   auto* msg = new pose_graph_msgs::PoseGraph;
-  msg->header.frame_id = pose_graph.fixed_frame_id;
+  msg->header.frame_id = fixed_frame_id;
   // TODO: get time stamp?
   msg->header.stamp =
-      pose_graph.keyed_stamps.at(pose_graph.key - 1); // Get timestamp from latest keyed pose
+      keyed_stamps.at(key - 1); // Get timestamp from latest keyed pose
 
   // Get Values
   // Converts the internal values
-  for (const auto& keyed_pose : pose_graph.values) {
-    gu::Transform3 t = utils::ToGu(pose_graph.values.at<gtsam::Pose3>(keyed_pose.key));
+  for (const auto& keyed_pose : values) {
+    gu::Transform3 t = utils::ToGu(values.at<gtsam::Pose3>(keyed_pose.key));
 
     gtsam::Symbol sym_key = gtsam::Symbol(keyed_pose.key);
 
     // Populate the message with the pose's data.
     GraphNode node;
     node.key = keyed_pose.key;
-    node.header.frame_id = pose_graph.fixed_frame_id;
+    node.header.frame_id = fixed_frame_id;
     node.pose = gr::ToRosPose(t);
 
     // Get timestamp
     // Note keyed_stamps are for all nodes TODO: check this is followed
     // TODO: check if time stamps are necessary
-    node.header.stamp = pose_graph.keyed_stamps.at(keyed_pose.key);
+    node.header.stamp = keyed_stamps.at(keyed_pose.key);
 
     // Get the IDs
-    if (pose_graph.keyed_scans.count(keyed_pose.key)) {
+    if (keyed_scans.count(keyed_pose.key)) {
       // Key frame, note in the ID
       node.ID = "key_frame";
-    } else if (sym_key.chr() == pose_graph.prefix[0]) {
+    } else if (sym_key.chr() == prefix[0]) {
       // Odom or key frame
       node.ID = "odom";
     } else if (sym_key.chr() == 'u') {
@@ -58,13 +62,10 @@ GraphPtr PoseGraphSerializer::ConvertToMsg(const PoseGraph& pose_graph) {
 
   // Add the factors  // TODO: check integration of this tracking with all
   // handlers
-  msg->edges = pose_graph.edges;
-  msg->priors = pose_graph.priors;
+  msg->edges = edges;
+  msg->priors = priors;
 
-  return GraphPtr(msg);
+  return GraphMsgPtr(msg);
 }
 
-bool ConvertFromMsg(const GraphPtr& msg,
-                    PoseGraph* pose_graph) {
-  return true;
-}
+bool PoseGraph::FromMsg(const GraphMsgPtr& msg) {}
