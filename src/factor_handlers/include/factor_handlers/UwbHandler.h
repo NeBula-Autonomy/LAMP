fd9d34fa28a87ef6a72647956f941e3f4acd585c
Nobuhiro Funabiki <nobuhiro.funabiki@jpl.nasa.gov>
2019-10-29 16:31:15 -0700

    First implementation of UwbHandler

---
+++ src/factor_handlers/include/factor_handlers/UwbHandler.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright Notes
+ *
+ * Authors: Nobuhiro Funabiki   (nobuhiro.funabiki@jpl.nasa.gov)
+*/
+
+#ifndef UWB_HANDLER_H
+#define UWB_HANDLER_H
+
+#include <factor_handlers/LampDataHandlerBase.h>
+
+class UwbHandler : public LampDataHandlerBase {
+
+    UwbHandler();
+    ~UwbHandler();
+};
+
+#endif
---
+++ src/factor_handlers/src/UwbHandler.cc
@@ -0,0 +1,11 @@
+/*
+ * Copyright Notes
+ *
+ * Authors: Nobuhiro Funabiki   (nobuhiro.funabiki@jpl.nasa.gov)
+*/
+
+#include <factor_handlers/UwbHandler.h>
+
+UwbHandler::UwbHandler() {}
+
+UwbHandler::~UwbHandler() {}
