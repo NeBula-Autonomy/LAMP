9f3daf997f66058df32be64cd2d3096642e7bafb
Alex Stephens <alex.stephens@jpl.nasa.gov>
2019-11-21 17:15:06 -0800

    Cleanup manual loop closure script

--- src/lamp/scripts/manual_LC_publish.py
+++ src/lamp/scripts/manual_LC_publish.py
@@ -43,30 +43,38 @@ def GetGtsamSymbol(symb):
     return join_pg_key(chr, ind)
 
 
-base = sys.argv[1]
-key_from = sys.argv[2]
-key_to = sys.argv[3]
+def PublishManualLoopClosure(base, key_from, key_to):
 
-string = """
-rostopic pub /{}/manual_loop_closure pose_graph_msgs/PoseGraph "header:
-  seq: 0
-  stamp: {{secs: 0, nsecs: 0}}
-  frame_id: ''
-incremental: false
-nodes: []
-edges:
-- key_from: {}
-  key_to: {}
-  type: 0
-  pose:
-    position: {{x: 0.0, y: 0.0, z: 0.0}}
-    orientation: {{x: 0.0, y: 0.0, z: 0.0, w: 0.0}}
-  covariance: [100, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 100, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-    100, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0001, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0001, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0, 0.0001]
-  range: 0.0
-  range_error: 0.0"
-""".format(base, GetGtsamSymbol(key_from), GetGtsamSymbol(key_to))
+    string = """
+    rostopic pub /{}/manual_loop_closure pose_graph_msgs/PoseGraph "
+    header:
+      seq: 0
+      stamp: {{secs: 0, nsecs: 0}}
+      frame_id: ''
+    incremental: false
+    nodes: []
+    edges:
+    - key_from: {}
+      key_to: {}
+      type: 0
+      pose:
+        position: {{x: 0.0, y: 0.0, z: 0.0}}
+        orientation: {{x: 0.0, y: 0.0, z: 0.0, w: 0.0}}
+      covariance: [100, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 100, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        100, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0001, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0001, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0001]
+      range: 0.0
+      range_error: 0.0"
+    """.format(base, GetGtsamSymbol(key_from), GetGtsamSymbol(key_to))
 
-print(string)
-os.system(string)
+    print(string)
+    os.system(string)
+
+
+if __name__ == '__main__':
+
+    base = sys.argv[1]
+    key_from = sys.argv[2]
+    key_to = sys.argv[3]
+
+    PublishManualLoopClosure(base, key_from, key_to)
