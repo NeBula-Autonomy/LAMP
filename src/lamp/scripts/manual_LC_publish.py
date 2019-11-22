569c2833184bb90e99e81030b5b8b62338e2de04
Alex Stephens <alex.stephens@jpl.nasa.gov>
2019-11-21 17:04:35 -0800

    Update manual loop closure python script

--- src/lamp/scripts/manual_LC_publish.py
+++ src/lamp/scripts/manual_LC_publish.py
@@ -12,20 +12,35 @@
 
 import sys
 import os
+import ctypes
+import numpy
 
-symbol_map = {'a': 6989586621679009792,
-              'b': 7061644215716937728,
-              'c': 7133701809754865664,
-              'd': 7205759403792793600,
-              'e': 7277816997830721536,
-              'f': 7349874591868649472}
+keyBits = ctypes.sizeof(ctypes.c_uint64) * 8
+chrBits = ctypes.sizeof(ctypes.c_ubyte) * 8
+indexBits = keyBits - chrBits
+chrMask = numpy.uint64(numpy.int64(~numpy.ubyte(0)) << indexBits)
+indexMask = ~chrMask
+
+
+def split_pg_key(pg_key):
+    c_ = chr(numpy.ubyte(numpy.int64(numpy.uint64(pg_key) & chrMask) >> indexBits))
+    j_ = numpy.uint64(pg_key) & indexMask
+    return c_, j_
+# In [79]: split_pg_key(7061644215716937728)
+# Out[79]: ('b', 0)
+
+
+def join_pg_key(c_, j_):
+    return numpy.uint64(ord(c_) << indexBits) + numpy.uint64(j_)
+# In [8]: join_pg_key('b',0)
+# Out[8]: 7061644215716937728
 
 
 def GetGtsamSymbol(symb):
     chr = symb[0]
     ind = int(symb[1:])
 
-    return symbol_map[chr] + ind
+    return join_pg_key(chr, ind)
 
 
 base = sys.argv[1]
@@ -50,8 +65,7 @@ edges:
     100, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0001, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0001, 0.0,
     0.0, 0.0, 0.0, 0.0, 0.0, 0.0001]
   range: 0.0
-  range_error: 0.0
-priors: []"
+  range_error: 0.0"
 """.format(base, GetGtsamSymbol(key_from), GetGtsamSymbol(key_to))
 
 print(string)
