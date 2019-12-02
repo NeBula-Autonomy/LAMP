98a2ae729fed38f91a022ab8ca816c36447dd0a1
Matteo Palieri <matteo.palieri@jpl.nasa.gov>
2019-12-02 11:44:22 -0800

    Add script to avoid duplicate messages on vn100/imu_wori_wcov topic caused by localizer_state_fusion

---
+++ scripts/filter_imu_bags.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+: '
+DOCUMENTATION: 
+    - Script to avoid duplicate messages on vn100/imu_wori_wcov topic 
+
+USAGE: 
+    - Add "source ~/Desktop/filter_imu_bags.sh" to .bashrc
+    - Run "filter_imu_bags husky#" in rosbag folder
+'
+
+filter_imu_bags()
+{ 
+  if [ "$1" != "" ]
+    then
+        echo 'Filtering imu bags retaining only vn100/imu topic'
+        rosbag filter $1_state_tcp_no_delay_*.bag $1_imu.bag "topic=='/$1/vn100/imu'"
+        mkdir imu_unused 
+        mv $1_state_tcp_no_delay_*.bag imu_unused/
+  else
+        echo "Please specify robot name"
+  fi
+}
