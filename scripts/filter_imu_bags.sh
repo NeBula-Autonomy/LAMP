08bab3e5f9d096f7152911e35baf927bff6f4d58
Matteo Palieri <matteo.palieri@jpl.nasa.gov>
2019-12-02 15:14:40 -0800

    Merge multiple state_tcp_no_delay bags with vn100/imu topic

--- scripts/filter_imu_bags.sh
+++ scripts/filter_imu_bags.sh
@@ -7,17 +7,14 @@ DOCUMENTATION:
 USAGE: 
     - Add "source ~/Desktop/filter_imu_bags.sh" to .bashrc
     - Run "filter_imu_bags husky#" in rosbag folder
-
-TODO:  
-    - Add support for multiple state_tcp_no_delay bags in rosfolder
 '
 
 filter_imu_bags()
 { 
   if [ "$1" != "" ]
     then
-        echo 'Filtering imu bags retaining only vn100/imu topic'
-        rosbag filter $1_state_tcp_no_delay_*.bag $1_imu.bag "topic=='/$1/vn100/imu'"
+        echo "Merging state_tcp_no_delay bags with vn100/imu topic"
+        python $MERGE_SCRIPT $1_state_tcp_no_delay_merged.bag $1_state_tcp_no_delay_* -t "/$1/vn100/imu"
         mkdir imu_unused 
         mv $1_state_tcp_no_delay_*.bag imu_unused/
   else
