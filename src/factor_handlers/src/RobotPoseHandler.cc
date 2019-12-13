80131267df1c70b17b05510677474e3a29a3a0d7
Alex Stephens <alex.stephens@jpl.nasa.gov>
2019-12-13 10:24:26 -0800

    Add RobotPoseHandler to CMakeLists

--- src/factor_handlers/src/RobotPoseHandler.cc
+++ src/factor_handlers/src/RobotPoseHandler.cc
@@ -106,7 +106,7 @@ void RobotPoseHandler::PoseCallback(const geometry_msgs::PoseStamped::ConstPtr& 
   ROS_INFO_STREAM("In RobotPoseHandler::PoseCallback");
 // void RobotPoseHandler::PoseCallback(const geometry_msgs::PoseStamped& msg, std::string robot) {
 
-  ROS_INFO_STREAM("Received pose from robot: " << robot);
+  // ROS_INFO_STREAM("Received pose from robot: " << robot);
 
   PoseData new_data;
   new_data.stamp = msg->header.stamp;
