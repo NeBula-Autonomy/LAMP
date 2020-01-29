bdc59de8ed6d0bfcf80d611d984d3455e9806264
bmorrell <benjamin.morrell@jpl.nasa.gov>
2020-01-29 00:27:54 -0800

    add unit test

--- src/lamp/test/test_lamp_base.cc
+++ src/lamp/test/test_lamp_base.cc
@@ -10,24 +10,128 @@
 
 class TestLampBase : public ::testing::Test {
 public:
-  PointCloud::Ptr data;
+  PointCloud::Ptr scan_;
+  pose_graph_msgs::PoseGraph graph_;
+  pose_graph_msgs::KeyedScan scan_msg_;
+  gtsam::Symbol init_key_;
+  pose_graph_msgs::PoseGraphNode n0, n1;
+  pose_graph_msgs::PoseGraphEdge e0;
 
-  TestLampBase() {
+  TestLampBase() : scan_(new PointCloud(3, 3)) {
+
+    //Load params
+    system("rosparam load $(rospack find "
+           "lamp)/config/precision_parameters.yaml");
+    system("rosparam load $(rospack find lamp)/config/lamp_frames_base.yaml");
+    system("rosparam load $(rospack find lamp)/config/lamp_rates.yaml");
+    system("rosparam load $(rospack find lamp)/config/lamp_init_noise.yaml");
+    system("rosparam load $(rospack find lamp)/config/lamp_settings.yaml");
+    system("rosparam load $(rospack find lamp)/config/GT_artifacts.yaml");
+    system("rosparam load $(rospack find lamp)/config/robot_names.yaml");
+    
+
+    system("rosparam set artifact_prefix 'l'");
+
+    system("rosparam load $(rospack find "
+           "factor_handlers)/config/manual_lc_parameters.yaml");
+    system("rosparam load $(rospack find "
+        "point_cloud_mapper)/config/parameters.yaml");
+
+    // Create data in the point cloud
+    int n_points = 3;
+    for (int i = 0; i < n_points; i++) {
+      for (int j = 0; j < n_points; j++) {
+        scan_->at(j, i).x = (float)j / (n_points - 1);
+        scan_->at(j, i).y = (float)i / (n_points - 1);
+        scan_->at(j, i).z = 0.0f;
+      }
+    }
+
+    // init_key_ = gtsam::Symbol('a', 0);
+    // // Make keyed scan
+    // scan_msg_->key = init_key_;
+    // pcl::toROSMsg(*scan_, scan_msg_->scan);
+
+    
+    n0.key = gtsam::Symbol('a', 0);
+    n0.pose.position.x = 0.0;
+    n0.pose.position.y = 0.0;
+    n0.pose.position.z = 0.0;
+
+    n1.key = gtsam::Symbol('a', 1);
+    n1.pose.position.x = 1.0;
+    n1.pose.position.y = 0.0;
+    n1.pose.position.z = 0.0;
+
+    e0.key_from = n0.key;
+    e0.key_to = n1.key;
+    e0.pose.position.x = 1.0;
+    e0.pose.position.y = 0.0;
+    e0.pose.position.z = 0.0;    
+
+    // graph_->nodes.push_back(n0);
 
   }
   ~TestLampBase() {}
 
-  void SetPoseGraph(PoseGraph graph) { lb.pose_graph_ = graph; }
-  PoseGraph GetPoseGraph() { return lb.pose_graph_; }
+  // void SetPoseGraph(PoseGraph graph) { lb.pose_graph_ = graph; }
+  // PoseGraph GetPoseGraph() { return lb.pose_graph_; }
   bool ProcessPoseGraphData(std::shared_ptr<FactorData> data) { return lb.ProcessPoseGraphData(data); }
 
+  int GetMapDataSize(){
+    return lb.mapper_.GetMapData()->size();
+  }
+
   LampBaseStation lb;
+
+  PoseGraphData data_;
 
   
 protected:
 
 private:
 };
+
+TEST_F(TestLampBase, Initialize){
+  ros::NodeHandle nh, pnh("~");
+  lb.Initialize(pnh, false /* online processing */);
+}
+
+TEST_F(TestLampBase, AddScansBeforePoseNodes){
+  ros::NodeHandle nh, pnh("~");
+  lb.Initialize(pnh, false /* online processing */);
+  
+  // Add keyed scans to map 
+  init_key_ = gtsam::Symbol('a', 0);
+  // Make keyed scan
+  scan_msg_.key = init_key_;
+  pcl::toROSMsg(*scan_, scan_msg_.scan);
+
+  data_.b_has_data = true;
+
+  pose_graph_msgs::KeyedScan::ConstPtr msg_ptr(new pose_graph_msgs::KeyedScan(scan_msg_));
+  data_.scans.push_back(msg_ptr);
+
+  // Process data
+  std::shared_ptr<PoseGraphData> data_shared = std::make_shared<PoseGraphData>(data_);
+  ProcessPoseGraphData(data_shared);
+  
+  // Add graph data
+  graph_.nodes.push_back(n0);
+  data_.scans.clear();
+  data_.b_has_data = true;
+
+  pose_graph_msgs::PoseGraph::ConstPtr msg_ptr2(new pose_graph_msgs::PoseGraph(graph_));
+  data_.graphs.push_back(msg_ptr2);
+
+  // Process data
+  data_shared = std::make_shared<PoseGraphData>(data_);
+  ProcessPoseGraphData(data_shared);
+
+  // Check map  
+  EXPECT_TRUE(GetMapDataSize() > 0);
+
+}
 
 TEST_F(TestLampBase, PoseGraphUpdateAfterOptimization) {
 
