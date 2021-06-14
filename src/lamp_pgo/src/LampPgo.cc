f8cfd017c14d83cb20512f46eef0a6596faa0a5a
user <yunchang@mit.edu>
2021-06-13 21:23:31 -0400

    isolate chris d lamp pgo fixes

--- src/lamp_pgo/src/LampPgo.cc
+++ src/lamp_pgo/src/LampPgo.cc
@@ -287,25 +287,46 @@ void LampPgo::PublishValues() const {
         values_.at<gtsam::Pose3>(key).rotation().toQuaternion().z();
     node.pose.orientation.w =
         values_.at<gtsam::Pose3>(key).rotation().toQuaternion().w();
-    // covariance
-    try {
-      auto cov_matrix = marginal.marginalCovariance(gtsam::Symbol(key));
-      int iter = 0;
-      for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 6; j++) {
-          node.covariance[iter] = cov_matrix(i, j);
-          iter++;
-        }
-      }
-    }
-    catch (std::exception& e) {
-      ROS_WARN_STREAM("Key is not found in the clique" << gtsam::DefaultKeyFormatter(key));
-    }
+
 
 
     // ROS_WARN_STREAM("Debug get covariance");
 
     pose_graph_msg.nodes.push_back(node);
+  }
+  try {
+    gtsam::Marginals marginal(nfg_, values_);
+    for (size_t k = 0 ; k < key_list.size(); ++k) {
+      auto key = key_list[k];
+      auto node = pose_graph_msg.nodes[k];
+      // covariance
+      try {
+        auto cov_matrix = marginal.marginalCovariance(gtsam::Symbol(key));
+        int iter = 0;
+        for (int i = 0; i < 6; i++) {
+          for (int j = 0; j < 6; j++) {
+            node.covariance[iter] = cov_matrix(i, j);
+            iter++;
+          }
+        }
+      }
+      catch (std::exception& e) {
+        ROS_WARN_STREAM("Key is not found in the clique" << gtsam::DefaultKeyFormatter(key));
+      }
+    }
+  } catch (gtsam::IndeterminantLinearSystemException e) {
+    ROS_ERROR_STREAM("LampPgo System is indeterminant, not computing covariance");
+    for (size_t k = 0 ; k < key_list.size(); ++k) {
+      auto key = key_list[k];
+      auto node = pose_graph_msg.nodes[k];
+      int iter = 0;
+      for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 6; j++) {
+          node.covariance[iter] = 1e-4;
+          iter++;
+        }
+      }
+    }
   }
 
   for (const auto& factor : nfg_) {
