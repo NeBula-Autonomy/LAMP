b338406349cbec378ffe84999d69bb2f3d0505ad
ychang <yunchang@mit.edu>
2021-07-16 09:40:42 -0700

    add missing rotation term in case of large rotation

--- src/loop_closure/src/PointCloudUtils.cc
+++ src/loop_closure/src/PointCloudUtils.cc
@@ -165,8 +165,9 @@ void ComputeAp_ForPoint2PlaneICP(const PointCloud::Ptr query_normalized,
     if (a_i.hasNaN() || n_i.hasNaN()) continue;
 
     Eigen::Matrix<double, 1, 6> H = Eigen::Matrix<double, 1, 6>::Zero();
-    H.block(0, 0, 1, 3) = (a_i.cross(n_i)).transpose();
-    H.block(0, 3, 1, 3) = n_i.transpose();
+    Eigen::Matrix3d R = T.block<3, 3>(0, 0).cast<double>();
+    H.block(0, 0, 1, 3) = (a_i.cross(R * n_i)).transpose();
+    H.block(0, 3, 1, 3) = (R * n_i).transpose();
     Ap += H.transpose() * H;
   }
   if (query_null) {
