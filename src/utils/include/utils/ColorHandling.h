5aa078f2d45390851369dda59f9b67eb3e93ea2a
Benjamin Morrell <benjamin.morrell@jpl.nasa.gov>
2021-08-18 06:06:55 -0700

    minor fixes

--- src/utils/include/utils/ColorHandling.h
+++ src/utils/include/utils/ColorHandling.h
@@ -5,17 +5,17 @@
 #include <pcl/impl/point_types.hpp>
 #include <string>
 const std::map<unsigned char, pcl::RGB> ROBOT_COLOR = {
-    {'a', pcl::GlasbeyLUT::at(0)},  //    {"husky1",  'a'},
-    {'b', pcl::GlasbeyLUT::at(1)},  //      {"husky2",  'b'},
-    {'c', pcl::GlasbeyLUT::at(2)},  //      {"husky3",  'c'},
-    {'d', pcl::GlasbeyLUT::at(3)},  //      {"husky4",  'd'},
-    {'e', pcl::GlasbeyLUT::at(4)},  //      {"spot1",'e'},
-    {'f', pcl::GlasbeyLUT::at(5)},  //      {"spot2",   'f'},
-    {'g', pcl::GlasbeyLUT::at(6)},  //      {"spot3",   'g'},
-    {'h', pcl::GlasbeyLUT::at(9)},  //      {"spot4",   'h'},
+    {'a', pcl::GlasbeyLUT::at(0)},   //    {"husky1",  'a'},
+    {'b', pcl::GlasbeyLUT::at(1)},   //      {"husky2",  'b'},
+    {'c', pcl::GlasbeyLUT::at(2)},   //      {"husky3",  'c'},
+    {'d', pcl::GlasbeyLUT::at(3)},   //      {"husky4",  'd'},
+    {'e', pcl::GlasbeyLUT::at(4)},   //      {"spot1",'e'},
+    {'f', pcl::GlasbeyLUT::at(5)},   //      {"spot2",   'f'},
+    {'g', pcl::GlasbeyLUT::at(6)},   //      {"spot3",   'g'},
+    {'h', pcl::GlasbeyLUT::at(9)},   //      {"spot4",   'h'},
     {'i', pcl::GlasbeyLUT::at(10)},  //      {"aquila1",   'i'},
-    {'j', pcl::GlasbeyLUT::at(10)}, //      {"aquila2", 'j' }
-    {'k', pcl::GlasbeyLUT::at{11}}, //      {"kaist1", 'j' }
-    {'l', pcl::GlasbeyLUT::at(12)}, //      {"zoe1", 'j' }
-    {'m', pcl::GlasbeyLUT::at(12)}, //      {"zoe2", 'j' }
+    {'j', pcl::GlasbeyLUT::at(10)},  //      {"aquila2", 'j' },
+    {'k', pcl::GlasbeyLUT::at(11)},  //      {"kaist1", 'j' },
+    {'l', pcl::GlasbeyLUT::at(12)},  //      {"zoe1", 'j' },
+    {'m', pcl::GlasbeyLUT::at(12)},  //      {"zoe2", 'j' },
     {'x', pcl::GlasbeyLUT::at(13)}}; //      {"xmaxx1", 'j' }
--- src/utils/include/utils/PrefixHandling.h
+++ src/utils/include/utils/PrefixHandling.h
@@ -24,40 +24,36 @@ namespace utils {
   const char UWB_PREFIX = 'u';
 
   // Define prefixes for ALL VALID ROBOTS in this file
-  const std::map<std::string, char> ROBOT_PREFIXES = {
-      {"husky1", 'a'},
-      {"husky2", 'b'},
-      {"husky3", 'c'},
-      {"husky4", 'd'},
-      {"spot1", 'e'},
-      {"spot2", 'f'},
-      {"spot3", 'g'},
-      {"spot4", 'h'},
-      {"aquila1", 'i'},
-      {"aquila2", 'j'},
-      {"kasit1", 'k'},
-      {"zoe1", 'l'},
-      {"zoe2", 'm'},
-      {"xmaxx1", 'x'},
-      {"base", 'z'},
-  };
+  const std::map<std::string, char> ROBOT_PREFIXES = {{"husky1", 'a'},
+                                                      {"husky2", 'b'},
+                                                      {"husky3", 'c'},
+                                                      {"husky4", 'd'},
+                                                      {"spot1", 'e'},
+                                                      {"spot2", 'f'},
+                                                      {"spot3", 'g'},
+                                                      {"spot4", 'h'},
+                                                      {"aquila1", 'i'},
+                                                      {"aquila2", 'j'},
+                                                      {"kasit1", 'k'},
+                                                      {"zoe1", 'l'},
+                                                      {"zoe2", 'm'},
+                                                      {"xmaxx1", 'x'},
+                                                      {"base", 'z'}};
 
-  const std::map<std::string, char> ARTIFACT_PREFIXES = {
-      {"husky1", 'A'},
-      {"husky2", 'B'},
-      {"husky3", 'C'},
-      {"husky4", 'D'},
-      {"spot1", 'E'},
-      {"spot2", 'F'},
-      {"spot3", 'G'},
-      {"spot4", 'H'},
-      {"aquila1", 'I'},
-      {"aquila2", 'J'},
-      {"kasit1", 'K'},
-      {"zoe1", 'L'},
-      {"zoe2", 'M'},
-      {"xmaxx1", 'X'},
-  };
+  const std::map<std::string, char> ARTIFACT_PREFIXES = {{"husky1", 'A'},
+                                                         {"husky2", 'B'},
+                                                         {"husky3", 'C'},
+                                                         {"husky4", 'D'},
+                                                         {"spot1", 'E'},
+                                                         {"spot2", 'F'},
+                                                         {"spot3", 'G'},
+                                                         {"spot4", 'H'},
+                                                         {"aquila1", 'I'},
+                                                         {"aquila2", 'J'},
+                                                         {"kasit1", 'K'},
+                                                         {"zoe1", 'L'},
+                                                         {"zoe2", 'M'},
+                                                         {"xmaxx1", 'X'}};
 
   // ---------------------------------------------------------
   //                    Query functions
@@ -71,107 +67,106 @@ namespace utils {
         return true;
       }
     }
-  }
-  return false;
-}
-
-// Checks if the character is an artifact prefix;
-inline bool IsArtifactPrefix(unsigned char c) {
-  for (auto k : ARTIFACT_PREFIXES) {
-    if (k.second == c) {
-      return true;
-    }
-  }
-  return false;
-}
-
-// Checks if the character is an artifact or UWB prefix;
-inline bool IsSpecialSymbol(unsigned char c) {
-  return IsArtifactPrefix(c) || (c == UWB_PREFIX);
-}
-
-// Get the prefix for the given robot
-inline unsigned char GetRobotPrefix(std::string robot) {
-  if (!ROBOT_PREFIXES.count(robot)) {
-    return 0;
-  }
-
-  return ROBOT_PREFIXES.at(robot);
-}
-
-// Get the artifact prefix for the given robot
-inline unsigned char GetArtifactPrefix(std::string robot) {
-  if (!ARTIFACT_PREFIXES.count(robot)) {
-    return 0;
-  }
-
-  return ARTIFACT_PREFIXES.at(robot);
-}
-
-// For a given node namespace (e.g. /husky1/lamp_pgo), returns the parameter
-// namespace that should be used ("base" or "robot")
-inline std::string GetParamNamespace(std::string ns) {
-  if (ns.find("base") != std::string::npos) {
-    return "base";
-  }
-
-  for (auto pair : ROBOT_PREFIXES) {
-    if (ns.find(pair.first) != std::string::npos) {
-      return "robot";
-    }
-  }
-
-  ROS_ERROR_STREAM("Namespace not recognized as base station or robot");
-  return "";
-}
-
-// ---------------------------------------------------------
-//                    Key comparison functions
-// ---------------------------------------------------------
-
-// Checks if two keys come from the same robot
-inline bool IsKeyFromSameRobot(gtsam::Symbol key1, gtsam::Symbol key2) {
-  if (!utils::IsRobotPrefix(key1.chr())) {
-    ROS_ERROR_STREAM(gtsam::DefaultKeyFormatter(key1)
-                     << " is not a valid robot key.");
-    return false;
-  } else if (!utils::IsRobotPrefix(key2.chr())) {
-    ROS_ERROR_STREAM(gtsam::DefaultKeyFormatter(key2)
-                     << " is not a valid robot key.");
     return false;
   }
 
-  return key1.chr() == key2.chr();
-}
-
-// ---------------------------------------------------------
-//                    Get full vectors
-// ---------------------------------------------------------
-
-// Get all the robot prefixes
-inline std::vector<char> GetAllRobotPrefixes() {
-  std::vector<char> output;
-  for (auto p : ROBOT_PREFIXES) {
-    output.push_back(p.second);
+  // Checks if the character is an artifact prefix;
+  inline bool IsArtifactPrefix(unsigned char c) {
+    for (auto k : ARTIFACT_PREFIXES) {
+      if (k.second == c) {
+        return true;
+      }
+    }
+    return false;
   }
-  return output;
-}
 
-// Get all the artifact prefixes
-inline std::vector<char> GetAllArtifactPrefixes() {
-  std::vector<char> output;
-  for (auto p : ARTIFACT_PREFIXES) {
-    output.push_back(p.second);
+  // Checks if the character is an artifact or UWB prefix;
+  inline bool IsSpecialSymbol(unsigned char c) {
+    return IsArtifactPrefix(c) || (c == UWB_PREFIX);
   }
-  return output;
-}
 
-// Get all the artifact and UWB prefixes
-inline std::vector<char> GetAllSpecialSymbols() {
-  std::vector<char> output = GetAllArtifactPrefixes();
-  output.push_back(UWB_PREFIX);
-  return output;
-}
+  // Get the prefix for the given robot
+  inline unsigned char GetRobotPrefix(std::string robot) {
+    if (!ROBOT_PREFIXES.count(robot)) {
+      return 0;
+    }
+
+    return ROBOT_PREFIXES.at(robot);
+  }
+
+  // Get the artifact prefix for the given robot
+  inline unsigned char GetArtifactPrefix(std::string robot) {
+    if (!ARTIFACT_PREFIXES.count(robot)) {
+      return 0;
+    }
+
+    return ARTIFACT_PREFIXES.at(robot);
+  }
+
+  // For a given node namespace (e.g. /husky1/lamp_pgo), returns the parameter
+  // namespace that should be used ("base" or "robot")
+  inline std::string GetParamNamespace(std::string ns) {
+    if (ns.find("base") != std::string::npos) {
+      return "base";
+    }
+
+    for (auto pair : ROBOT_PREFIXES) {
+      if (ns.find(pair.first) != std::string::npos) {
+        return "robot";
+      }
+    }
+
+    ROS_ERROR_STREAM("Namespace not recognized as base station or robot");
+    return "";
+  }
+
+  // ---------------------------------------------------------
+  //                    Key comparison functions
+  // ---------------------------------------------------------
+
+  // Checks if two keys come from the same robot
+  inline bool IsKeyFromSameRobot(gtsam::Symbol key1, gtsam::Symbol key2) {
+    if (!utils::IsRobotPrefix(key1.chr())) {
+      ROS_ERROR_STREAM(gtsam::DefaultKeyFormatter(key1)
+                       << " is not a valid robot key.");
+      return false;
+    } else if (!utils::IsRobotPrefix(key2.chr())) {
+      ROS_ERROR_STREAM(gtsam::DefaultKeyFormatter(key2)
+                       << " is not a valid robot key.");
+      return false;
+    }
+
+    return key1.chr() == key2.chr();
+  }
+
+  // ---------------------------------------------------------
+  //                    Get full vectors
+  // ---------------------------------------------------------
+
+  // Get all the robot prefixes
+  inline std::vector<char> GetAllRobotPrefixes() {
+    std::vector<char> output;
+    for (auto p : ROBOT_PREFIXES) {
+      output.push_back(p.second);
+    }
+    return output;
+  }
+
+  // Get all the artifact prefixes
+  inline std::vector<char> GetAllArtifactPrefixes() {
+    std::vector<char> output;
+    for (auto p : ARTIFACT_PREFIXES) {
+      output.push_back(p.second);
+    }
+    return output;
+  }
+
+  // Get all the artifact and UWB prefixes
+  inline std::vector<char> GetAllSpecialSymbols() {
+    std::vector<char> output = GetAllArtifactPrefixes();
+    output.push_back(UWB_PREFIX);
+    return output;
+  }
 
 } // namespace utils
 
