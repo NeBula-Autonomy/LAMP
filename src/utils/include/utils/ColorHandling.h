c6b75044f205e84ca903e7bc5101bdbc882cf554
Benjamin Morrell <benjamin.morrell@jpl.nasa.gov>
2021-08-18 05:57:27 -0700

    Update ColorHandling.h

--- src/utils/include/utils/ColorHandling.h
+++ src/utils/include/utils/ColorHandling.h
@@ -8,10 +8,14 @@ const std::map<unsigned char, pcl::RGB> ROBOT_COLOR = {
     {'a', pcl::GlasbeyLUT::at(0)},  //    {"husky1",  'a'},
     {'b', pcl::GlasbeyLUT::at(1)},  //      {"husky2",  'b'},
     {'c', pcl::GlasbeyLUT::at(2)},  //      {"husky3",  'c'},
-    {'f', pcl::GlasbeyLUT::at(3)},  //      {"husky4",  'f'},
-    {'d', pcl::GlasbeyLUT::at(4)},  //      {"telemax1",'d'},
-    {'e', pcl::GlasbeyLUT::at(5)},  //      {"robot",   'e'},
-    {'g', pcl::GlasbeyLUT::at(0)},  //      {"spot1",   'g'},
-    {'h', pcl::GlasbeyLUT::at(1)},  //      {"spot2",   'h'},
-    {'i', pcl::GlasbeyLUT::at(8)},  //      {"handheld1",   'i'},
-    {'j', pcl::GlasbeyLUT::at(9)}}; //{ "xmaxx1", 'j' }
+    {'d', pcl::GlasbeyLUT::at(3)},  //      {"husky4",  'd'},
+    {'e', pcl::GlasbeyLUT::at(4)},  //      {"spot1",'e'},
+    {'f', pcl::GlasbeyLUT::at(5)},  //      {"spot2",   'f'},
+    {'g', pcl::GlasbeyLUT::at(6)},  //      {"spot3",   'g'},
+    {'h', pcl::GlasbeyLUT::at(9)},  //      {"spot4",   'h'},
+    {'i', pcl::GlasbeyLUT::at(10)},  //      {"aquila1",   'i'},
+    {'j', pcl::GlasbeyLUT::at(10)}, //      {"aquila2", 'j' }
+    {'k', pcl::GlasbeyLUT::at{11}}, //      {"kaist1", 'j' }
+    {'l', pcl::GlasbeyLUT::at(12)}, //      {"zoe1", 'j' }
+    {'m', pcl::GlasbeyLUT::at(12)}, //      {"zoe2", 'j' }
+    {'x', pcl::GlasbeyLUT::at(13)}}; //      {"xmaxx1", 'j' }
